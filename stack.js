2f8de1711c49d4141aa83a3f1b6129742fa6f47d
Zane <zane.zhao@chinaentropy.com>
2022-06-08 18:04:06 +0800

    栈

---
+++ stack.js
@@ -0,0 +1,42 @@
+// 栈 - 对象形式
+class Stack {
+  constructor() {
+    this.count = 0;
+    this.items = {};
+  }
+
+  push(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return this.count === 0;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.count - 1];
+  }
+
+  clear() {
+    this.count = 0;
+    this.items = {};
+  }
+}
