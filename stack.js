0d93f9e29d1174b7921226cc83bc9b8eb69447d1
Zane <zane.zhao@chinaentropy.com>
2022-06-10 09:52:13 +0800

    利用栈实现进制转换

--- stack.js
+++ stack.js
@@ -40,3 +40,57 @@ class Stack {
     this.items = {};
   }
 }
+
+// 利用栈完成十进制转二进制
+function decimalToBinary(decNumber) {
+  const remStack = new Stack();
+  let number = decNumber;
+  let rem = 0;
+  let binaryString = "";
+
+  while (number > 0) {
+    rem = number % 2;
+    remStack.push(rem);
+    number = Math.floor(number / 2);
+  }
+
+  while (!remStack.isEmpty()) {
+    binaryString += remStack.pop().toString();
+  }
+
+  return binaryString;
+}
+
+console.log(decimalToBinary(233)); // 11101001
+console.log(decimalToBinary(10)); // 1010
+console.log(decimalToBinary(1000)); // 1111101000
+
+// 利用栈完成十进制向2-36进制转换
+function baseConverter(decNumber, base) {
+  const remStack = new Stack();
+  const digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let number = decNumber;
+  let rem = 0;
+  let baseString = "";
+
+  if (base < 2 || base > 36) {
+    return "";
+  }
+
+  while (number > 0) {
+    rem = number % base;
+    remStack.push(rem);
+    number = Math.floor(number / base);
+  }
+
+  while (!remStack.isEmpty()) {
+    baseString += digits[remStack.pop()];
+  }
+
+  return baseString;
+}
+
+console.log(baseConverter(100345, 2)); // 11000011111111001
+console.log(baseConverter(100345, 8)); // 303771
+console.log(baseConverter(100345, 16)); // 187F9
+console.log(baseConverter(100345, 35)); // 2BW0
